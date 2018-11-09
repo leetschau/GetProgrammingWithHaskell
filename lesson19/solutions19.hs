7c81b4fad56caae1cb4de8a33f8553734e832287
Li Chao <leetschau@gmail.com>
2018-11-09 18:06:35 +0800

    more codes of Lesson 19

--- lesson19/solutions19.hs
+++ lesson19/solutions19.hs
@@ -25,6 +25,25 @@ organInventory = Map.fromList (zip uniqItems itemCnt)
         itemIdx = map (`elemIndices` items) uniqItems
         itemCnt = map length itemIdx
 
+-- 用反斜杠包裹函数名，将一个接收2个参数的前缀函数转换为一个中缀函数，
+-- 所以 (`elemIndices` items) 可以看作是 \x -> elemIndices x items 的简写
+-- 参考本书 6.3.4 elem 一节。
+
+possibleDrawers :: [Int]
+possibleDrawers = [1 .. 50]
+
+getDrawerContents :: [Int] -> Map.Map Int Organ -> [Maybe Organ]
+getDrawerContents ids catalog = map getContents ids
+  where getContents = \id -> Map.lookup id catalog
+
+availableOrgans :: [Maybe Organ]
+availableOrgans = getDrawerContents possibleDrawers organCatalog
+
+countOrgan :: Organ -> [Maybe Organ] -> Int
+countOrgan organ available = length (filter
+                                            (\x -> x == Just organ)
+                                            available)
+
 -- QC 19.2
 
 numOrZero :: Maybe Int -> Int
