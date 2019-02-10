2409d3d5376fb3adb6d159e56c966c1b054b330f
Li Chao <leetschau@gmail.com>
2019-02-10 18:01:18 +0800

    lesson 26 finished

--- lesson25/q251.hs
+++ lesson25/q251.hs
@@ -6,7 +6,7 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 import System.Environment
-import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString as B
 import qualified Data.Text as T
 import qualified Data.Text.IO as TI
 import qualified Data.Text.Encoding as E
@@ -15,7 +15,7 @@ getCharCounts :: T.Text -> Int
 getCharCounts text = T.length text
 
 getByteCounts :: T.Text -> Int
-getByteCounts text = BC.length bytes
+getByteCounts text = B.length bytes
   where bytes = E.encodeUtf8 text
 
 getCharByteCounts :: T.Text -> String
@@ -30,3 +30,8 @@ main = do
   let fileName = head args
   input <- TI.readFile fileName
   print (getCharByteCounts input)
+
+{- 这里的解法是先按 UTF-8 编码读取文件，算出字符数，再编码为字节数组，算出字节数；
+ - 答案则反过来：先读入字节数组，算出字节数，再以 UTF-8 解码为文本，算出字符数。
+ - 前者做了一次解码一次编码，后者只做了一次解码，更高效。
+-}
