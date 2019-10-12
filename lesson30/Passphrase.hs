6fe491fbef166c5fd5fc48f06168f7f912a9f5ea
Li Chao <leetschau@gmail.com>
2019-10-12 18:13:49 +0800

    add some notes about monad transformers

---
+++ lesson30/Passphrase.hs
@@ -0,0 +1,85 @@
+-- 基于 Monad transformers 一文中例子，详见 notes.md 文件
+import Data.Char
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Class
+
+main :: IO ()
+main = askPassphrase2
+
+-- An example validation test implementaton
+isValid :: String -> Bool
+isValid s = length s >= 8
+            && any isAlpha s
+            && any isNumber s
+            && any isPunctuation s
+
+-- 嵌套 Monad 实现 --
+
+askPassphrase2 :: IO ()
+askPassphrase2 = do
+  putStrLn "Insert your new passphrase:"
+  maybe_value <- getPassphrase2
+  -- 第一次拆箱，从 IO(Maybe String) 变成 Maybe String
+  case maybe_value of
+  -- 用 pattern matching 做第二次拆箱
+    Just value -> do
+      putStrLn $ "Your passphrase is: " ++ value
+      putStrLn "Storing in database..."
+    Nothing -> putStrLn "Passphrase invalid."
+
+getPassphrase2 :: IO (Maybe String)
+getPassphrase2 = do
+  s <- getLine
+  if isValid s then return $ Just s
+               else return Nothing
+
+-- Monad Transformer 实现 --
+
+newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
+-- 此处使用了 record 语法，MaybeT 是一个 record，包含一个名为 runMaybeT 的字段，
+-- 类型为 m (Maybe a)，MaybeT 的 kind 为 (* -> *) -> * -> * （ghci 中执行 :k MaybeT）
+
+instance Monad m => Monad (MaybeT m) where
+  return  = MaybeT . return . Just
+
+  -- The signature of (>>=), specialized to MaybeT m:
+  -- (>>=) :: MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+  -- 所以 f 的类型是 a -> MaybeT m b，f value 的类型是 MaybeT m b
+  -- runMaybeT 的类型是 MaybeT m a -> m (Maybe a)
+  -- 所以 runMaybeT $ f value 的类型是 m (Maybe b)（用类型变量 b 替换了 a）
+  x >>= f = MaybeT $ do maybe_value <- runMaybeT x
+                        case maybe_value of
+                           Nothing    -> return Nothing
+                           Just value -> runMaybeT $ f value
+
+instance Monad m => Applicative (MaybeT m) where
+    pure = return
+    (<*>) = ap
+
+instance Monad m => Functor (MaybeT m) where
+    fmap = liftM
+
+instance Monad m => Alternative (MaybeT m) where
+    empty   = MaybeT $ return Nothing
+    x <|> y = MaybeT $ do maybe_value <- runMaybeT x
+                          case maybe_value of
+                               Nothing    -> runMaybeT y
+                               Just _     -> return maybe_value
+
+instance Monad m => MonadPlus (MaybeT m) where 
+    mzero = empty
+    mplus = (<|>)
+
+instance MonadTrans MaybeT where
+    lift = MaybeT . (liftM Just)
+
+getPassphrase :: MaybeT IO String
+getPassphrase = do s <- lift getLine
+                   guard (isValid s) -- Alternative provides guard.
+                   return s
+
+askPassphrase :: MaybeT IO ()
+askPassphrase = do lift $ putStrLn "Insert your new passphrase:"
+                   value <- getPassphrase
+                   lift $ putStrLn "Storing in database..."
---
+++ lesson30/notes.md
@@ -0,0 +1,72 @@
+# Type Class, Functor, Applicative and Monad
+
+Haskell 的 Type 是一组值的集合，可能是有限个（例如布尔值），也可能是无限个（例如整数集），
+Type class 是函数（或者叫运算）的集合，例如 `Eq` 包含相等 `(==)` 和不等 `(/=)` 两种运算，
+如果一个 Type `a` 的值支持 class `A` 中定义的所有运算，则称 `a` 是 `A` 的一个 *instance*，
+`instance A a where` 下面要给出 `A` 中定义的所有函数在 `a` 上实现的方法。
+
+从编程者的角度看，`newtype` 和 `data` 没有区别，但对于编译器来说二者区别很大。
+例如对于定义 `data Foo = Foo Int`，`Foo 3` 和 `3` 是两个完全不同类型的值，
+而对于 `newtype Bar = Bar Int`，虽然编译时 `Bar 3` 和 `3` 类型不同，
+但运行时 `Bar 3` 会被转换为 `3` 参与计算。
+
+P.S.: 要在 ghci 里运行上述代码，需要写成 `data Foo = Foo Int deriving Show`
+      才能运行 `Foo 3`.
+
+另外 `data` 可以有多个 constructor，例如 `data Either a b = Left a | Right b`,
+`newtype` 只能有一个 constructor.
+
+Monad 的作用是保证内部的表达式被 **顺序执行**，而不是默认的惰性执行。
+因为在 Monad 场景中（例如文件 IO），顺序是很重要的。
+
+Monad 是一个 type class，它继承了 Applicative，Applicative 又继承自 functor，
+三者之间的关系见 30.3 节，以及 30.1 节的图示。
+
+这种继承关系导致 Monad 具有最广泛的处理 context 中信息的能力，
+因为一个 monad 也有 functor 的 `fmap` 和 applicative 的 `<*>` 方法，
+所有不纯粹 (pure) 的计算都可以用 monad 解决。
+
+Monad 的 `>>` 方法对应 Applicative 的 `*>` 方法，
+Monad 的 `return` 方法对应 Applicative 的 `pure` 方法。
+Monad 的 `ap` 方法对应 Applicative 的 `<*>` 方法。
+Monad 的 `liftM` 方法对应 Functor 的 `fmap` 方法。
+Monad 的 `return` 方法对应 Applicative 的 `pure` 方法。
+Monad 的 `return` 方法对应 Applicative 的 `pure` 方法。
+
+## Monad 与 命令式语言
+
+所谓命令式语言，指使用大括号和分号组织的，以带副作用的语句 (statement) 为基本单位的编程语言。
+
+*Notions of Computation* in [Understanding monads] 定义了命令式代码和 monad 代码间互相转换的方法，
+以及7种具体 Monad 实现（例如 Maybe, IO, 列表等）在 Haskell 和命令式语言中的实现方法。
+
+## Monad 与 Category Theory
+
+详见 *Monads and Category Theory* in [Understanding monads] 一节。
+
+将 monad 理解成一个箱子，则 monad 核心的三个函数可以看作：
+
+* `fmap`: 对箱子里的值应用函数；
+
+* `return`: 将一个纯粹值装箱；
+
+* `join`: 将嵌套的多个（可能不同类型的）箱子合并成一个箱子。
+
+在此基础上定义 *将多个处理步骤连接在一起*：`m >>= g = join (fmap g m)`。
+
+# Monad Transformers
+
+The whole point of monad transformers is that *they transform monads into monads*.
+
+P.S.:
+[Monad Transformers Step by Step] 给出了一个从纯函数式实现到 monad，再到 monad transformers 的演进实例，
+可惜代码在当前的 Haskell 版本下无法运行。
+
+参考文献：
+
+* [Understanding monads]: https://en.wikibooks.org/wiki/Haskell/Understanding_monads
+
+* [Monad transformers]: https://en.wikibooks.org/wiki/Haskell/Monad_transformers
+
+* [Monad Transformers Step by Step]: https://page.mi.fu-berlin.de/scravy/realworldhaskell/materialien/monad-transformers-step-by-step.pdf
+
